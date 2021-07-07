5e7bfdadf0145754b303b4f2b0752b5a79abb5ce
Chintan Sudani <csudani7@gmail.com>
2021-07-07 12:21:50 +0530

    fix: updated XTSCOnnection Component & Added comments

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,20 +3,13 @@ import { renderRoutes } from "react-router-config";
 import { BrowserRouter } from "react-router-dom";
 
 import { routes } from "../routes";
-import isNull from "lodash";
-
-import SocketHandler from "../components/SocketHandler";
 
 import "./app.css";
 
 export default function App() {
-  const isAuthenticated = localStorage.getItem("isUserLogged");
   return (
     <BrowserRouter>
       {renderRoutes(routes)}
-      {isAuthenticated === "true" && !isNull(isAuthenticated) && (
-        <SocketHandler />
-      )}
     </BrowserRouter>
   );
 }
--- src/services/XTSConnection/index.js
+++ src/services/XTSConnection/index.js
@@ -1,46 +1,49 @@
-var XtsMarketDataAPI = require("xts-marketdata-api").XtsMarketDataAPI;
-var XtsMarketDataWS = require("xts-marketdata-api").WS;
+let XtsMarketDataAPI = require("xts-marketdata-api").XtsMarketDataAPI;
+let XtsMarketDataWS = require("xts-marketdata-api").WS;
 
-let xtsUrl = process.env.REACT_APP_XTS_URL;
-let xtsSecretKey = process.env.REACT_APP_XTS_SECRETKEY;
-let xtsAppKey = process.env.REACT_APP_XTS_APPKEY;
-let xtsWebSource = process.env.REACT_APP_XTS_SOURCE;
-var xtsMarketDataAPI = null;
-var xtsMarketDataWS = null;
+const xtsUrl = process.env.REACT_APP_XTS_URL;
+const xtsSecretKey = process.env.REACT_APP_XTS_SECRETKEY;
+const xtsAppKey = process.env.REACT_APP_XTS_APPKEY;
+const xtsWebSource = process.env.REACT_APP_XTS_SOURCE;
+let xtsMarketDataAPI = null;
+let xtsMarketDataWS = null;
 
 export default class DataFeed {
   async callMasterAPI(settingLiveData) {
+    //XTS API Login Request
     xtsMarketDataAPI = new XtsMarketDataAPI(xtsUrl);
-    var loginRequest = {
+    let loginRequest = {
       secretKey: xtsSecretKey,
       appKey: xtsAppKey,
       source: xtsWebSource,
     };
     let logIn = await xtsMarketDataAPI.logIn(loginRequest);
+
     if (logIn && logIn.type === xtsMarketDataAPI.responseTypes.success) {
       let userID = logIn?.result?.userID;
+
+      //XTS WebSocket Connection Request
       xtsMarketDataWS = new XtsMarketDataWS(xtsUrl);
-      var socketInitRequest = {
+      const socketInitRequest = {
         userID: userID,
         publishFormat: "JSON",
         broadcastMode: "Full",
         token: logIn?.result?.token, // Token Generated after successful LogIn
       };
       xtsMarketDataWS.init(socketInitRequest);
-      await this.fetch_live_data_based_on_token(settingLiveData, 1502);
-      await this.registerEvents(1502);
+
+      //Subscription of each Instruments
+      await this.subscribeInstrument(settingLiveData);
+      //Fetch each MarketDepthData for each Instruments
+      await this.registerEvents();
     } else {
       console.error(logIn, "Error in XTS Login");
     }
   }
 
-  async subscription(subscriptionRequest) {
-    return await xtsMarketDataAPI.subscription(subscriptionRequest);
-  }
-
-  async fetch_live_data_based_on_token(settingLiveData) {
-    var instrumentsData = [];
-    for (var i = 0; i < settingLiveData.length; ++i) {
+  async subscribeInstrument(settingLiveData) {
+    let instrumentsData = [];
+    for (let i = 0; i < settingLiveData.length; ++i) {
       if (settingLiveData[i].ExchangeSegment === "NSECM") {
         instrumentsData.push({
           exchangeSegment: 1,
@@ -60,6 +63,8 @@ export default class DataFeed {
         console.log("Sorry! Something went wrong!!!");
       }
     }
+
+    //Subscription of each instruments
     let subscriptionRequest = {
       instruments: instrumentsData,
       xtsMessageCode: 1502, //1502: Market Data
@@ -67,6 +72,12 @@ export default class DataFeed {
     await this.subscription(subscriptionRequest);
   }
 
+  //Subscription Method
+  async subscription(subscriptionRequest) {
+    return await xtsMarketDataAPI.subscription(subscriptionRequest);
+  }
+
+  //XTS Websocket events to find market data
   async registerEvents() {
     xtsMarketDataWS.onConnect((connectData) => {
       console.log(connectData, "connectData");
@@ -88,8 +99,8 @@ export default class DataFeed {
       console.log(disconnectData, "disconnectData");
     });
 
-    // xtsMarketDataWS.onLogout((logoutData) => {
-    //   console.log(logoutData, "logoutData");
-    // });
+    xtsMarketDataWS.onLogout((logoutData) => {
+      console.log(logoutData, "logoutData");
+    });
   }
 }
